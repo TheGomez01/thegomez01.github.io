1032f50bd26a88f3234f2c2f94aff31c9f6257ba
TheGomez01 <159203345+TheGomez01@users.noreply.github.com>
2025-03-11 17:09:30 -0500

    Create index.js

---
+++ index.js
@@ -0,0 +1,7 @@
+let isMobile = /iPhone|iPad|iPod|Android/i.test(navigator.userAgent);
+
+if (isMobile) {
+  /* set navbar class to mobile one */
+} else {
+  /* set navbar class to desktop one */
+}
