cb74ad9e8d3b2527fb8d1c582ef3e9c4b623845e
TheGomez01 <159203345+TheGomez01@users.noreply.github.com>
2025-03-24 17:12:00 -0500

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ let isMobile = /iPhone|iPad|iPod|Android/i.test(navigator.userAgent);
 
 if (isMobile) {
   /* set navbar class to mobile one */
-  alert("Hey! This website is still under construction. Currently, it blows major ass on mobile devices. Please view the website from a desktop or laptop. Thanks!")
+  alert("Hey! This website is still under construction. Currently, the experience on mobile devices is mediocre. Please view the website from a desktop or laptop. Thanks!")
 } else {
   /* set navbar class to desktop one */
 }
